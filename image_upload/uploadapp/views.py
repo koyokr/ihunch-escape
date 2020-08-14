2655ef7039f021a2d04aa1ec93222eb116d77aa2
soledo <volea5aka5@gmail.com>
2020-08-14 22:35:33 +0900

    Upload djangoproject

---
+++ image_upload/uploadapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import File
+from .serializers import FileSerializer
+# Create your views here.
+
+class iHunchPredictor:
+    def predict(self, img_bytes):
+        return {"ihunch": 0.0}
+
+
+class FileUploadView(APIView):
+    parser_class = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = FileSerializer(data=request.FILES)
+        if file_serializer.is_valid():
+            img_bytes = request.FILES['file'].read()
+            pred = iHunchPredictor().predict(img_bytes)
+            return Response(pred, status=status.HTTP_200_OK)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
