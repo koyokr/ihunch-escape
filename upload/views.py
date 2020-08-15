00b33ca11053a8b1e477c2c479e25a335901d905
Younghun Ko <koyo@koyo.kr>
2020-08-15 19:55:39 +0900

    Fix iHunchPredictor() to self.predictor
    매 요청마다 인스턴스를 다시 생성하지 않도록 수정

--- upload/views.py
+++ upload/views.py
@@ -11,10 +11,14 @@ from .predictor import iHunchPredictor
 class Upload(APIView):
     parser_class = (FileUploadParser,)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.predictor = iHunchPredictor()
+
     def post(self, request, format=None):
         serializer = UploadSerializer(data=request.FILES)
         if serializer.is_valid():
             img_bytes = request.FILES['file'].read()
-            pred = iHunchPredictor().predict(img_bytes)
+            pred = self.predictor.predict(img_bytes)
             return Response(pred, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
